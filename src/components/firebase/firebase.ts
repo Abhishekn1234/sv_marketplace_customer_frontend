acfde4130f80c734312cefc89c59b282e44475dd
Abhishek N <abhishekpes123@gmail.com>
2026-04-29 17:12:30 +0530

    `Update .env and firebase.ts to use environment variables`

--- src/components/firebase/firebase.ts
+++ src/components/firebase/firebase.ts
@@ -4,12 +4,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyChCuX9ZrzrZUmeSc7WO-3Nalq8t84Yjyo",
-  authDomain: "sv-marketplace-46503.firebaseapp.com",
-  projectId: "sv-marketplace-46503",
-  storageBucket: "sv-marketplace-46503.appspot.com",
-  messagingSenderId: "118069674424",
-  appId: "1:118069674424:web:c21a0a1edbb9e808a94f4d",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
