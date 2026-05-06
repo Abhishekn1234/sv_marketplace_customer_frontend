aae45caed088b6c679f6ffb70790d8585e6bf9b7
Abhishek N <abhishekpes123@gmail.com>
2026-05-06 10:06:16 +0530

    `Updated UserIcon component and icons index to fix type annotations`

--- src/components/icons/Usericon.tsx
+++ src/components/icons/Usericon.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 
-export function UserIcon(props: React.SVGProps<SVGSVGElement>) {
+export function UserIcon(props?: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,8 +1,10 @@
+// SVG Icons (from files)
 import GoogleIcon from "@/components/icons/google.svg?react";
 import FacebookIcon from "@/components/icons/facebook.svg?react";
 import MoneyIcon from "@/components/icons/moneybill.svg?react";
-import ReactIcon from "@/components/icons/react.svg";
-// import ViteIcon from "@/components/icons/vite.svg?react";
+import ReactIcon from "@/components/icons/react.svg?react";
+
+// Custom Icons (TSX components)
 import { QuickActionsIcon } from "./QuickActionsIcon";
 import { SecurityIcon } from "./SecurityIcon";
 import { BackArrowIcon } from "./BackArrowIcon";
@@ -11,15 +13,35 @@ import { HelpIcon } from "./HelpIcon";
 import { PreferencesIcon } from "./PreferencesIcon";
 import { PrivacyIcon } from "./PrivacyIcon";
 import { LogoIcon } from "./LogoIcon";
-import {SaveIcon} from "./SaveIcon";
+import { SaveIcon } from "./SaveIcon";
 import { DangerZoneIcon } from "./DangerZoneIcon";
 import { DeleteAccountIcon } from "./DeleteAccountIcon";
 import { EmailNotificationIcon } from "./EmailNotificationIcon";
 import { SmsNotificationIcon } from "./SmsNotificationIcon";
 import { TwoFactorAuthIcon } from "./TwoFactorAuthIcon";
-import {UserIcon} from "./UserIcon";
+import { UserIcon } from "./UserIcon";
 import { ArrowRight } from "./ArrowRight";
 
-export { GoogleIcon,UserIcon, FacebookIcon,PrivacyIcon,LogoIcon,DangerZoneIcon,DeleteAccountIcon,EmailNotificationIcon,SmsNotificationIcon,TwoFactorAuthIcon, MoneyIcon,ReactIcon,QuickActionsIcon,SecurityIcon,BackArrowIcon,BillingIcon,HelpIcon ,PreferencesIcon,SaveIcon, ArrowRight };
-
-
+// Export everything
+export {
+  GoogleIcon,
+  FacebookIcon,
+  MoneyIcon,
+  ReactIcon,
+  QuickActionsIcon,
+  SecurityIcon,
+  BackArrowIcon,
+  BillingIcon,
+  HelpIcon,
+  PreferencesIcon,
+  PrivacyIcon,
+  LogoIcon,
+  SaveIcon,
+  DangerZoneIcon,
+  DeleteAccountIcon,
+  EmailNotificationIcon,
+  SmsNotificationIcon,
+  TwoFactorAuthIcon,
+  UserIcon,
+  ArrowRight,
+};
